6ce89abaddfda7e32a2d79c7659d663a9d6b0f36
Bernardo <bemascarenhas6@gmail.com>
2024-05-17 11:18:06 -0300

    gloria

--- Jogo/src/main.cpp
+++ Jogo/src/main.cpp
@@ -2,14 +2,38 @@
 
 int main()
 {
-   InitWindow(800, 450, "Hello, Raylib!");
+    
+   InitWindow(800, 800, "Hello, Raylib!");
     SetTargetFPS(60);
-
+    int x,y;
+    x = 400;
+    y = 400;
+    
     while (!WindowShouldClose())
     {
+       
+        if (IsKeyDown(KEY_UP))
+        {   
+             y-=10;
+            
+        }
+        if (IsKeyDown(KEY_DOWN))
+        {
+            y+=10;
+        }
+        if (IsKeyDown(KEY_LEFT))
+        {
+            x-=10;
+        }
+        if (IsKeyDown(KEY_RIGHT))
+        {
+            x+=10;
+        }
+
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello, World!", 10, 10, 20, DARKGRAY);
+        ClearBackground(WHITE);
+        DrawCircle(x, y, 50, RED);
         EndDrawing();
     }
     CloseWindow();
